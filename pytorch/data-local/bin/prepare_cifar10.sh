bd4313d5691f3ce4c30635e50fa207f49edf16fe
Vik Kamath <vikram@cai.fi>
2018-05-31 14:09:17 +0300

    Add license information
    - Addresses Issue #15

--- pytorch/data-local/bin/prepare_cifar10.sh
+++ pytorch/data-local/bin/prepare_cifar10.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2018, Curious AI Ltd. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
--- tensorflow/train_cifar10.py
+++ tensorflow/train_cifar10.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2018, Curious AI Ltd. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
 """Train ConvNet Mean Teacher on CIFAR-10 training set and evaluate against a validation set
 
 This runner converges quickly to a fairly good accuracy.
